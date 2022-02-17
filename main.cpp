cd616e5f6ca1a579b626b11a5430bba19d47c7df
Andrew Slabko <andrew@slabko.com>
2022-02-17 22:36:19 +0000

    add simple example

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 #include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
+
 int main() {
+  auto j = json::parse(R"({"happy": true, "pi": 3.141})");
+  std::cout << j["pi"].get<float>() << std::endl;
   return 0;
 }
